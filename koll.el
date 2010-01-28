0ce3208a302f8e1d0873394367f1c8c3ea518fcc
krille <krille@mamma.(none)>
2010-01-28 01:47:06 +0100

    first commit

---
+++ koll.el
@@ -0,0 +1,27 @@
+;; Author : Kristoffer Ström
+;; License: public domain
+
+;; very simple program to create a logfile of files worked on
+;; can be used to keep track of work related projects for billing
+
+;; just put this in elisp path and (require 'koll) in your config to have a go.
+
+;; alist containing latest file writeout
+(setf *koll-latest* nil
+      ;; koll interval is the number of seconds that should pass between 
+      ;; log entries of the same file. default 10 minutes.
+      *koll-interval* (* 60 10)
+      *koll-logfile* "~/.koll")
+
+(defun koll-hook ()  
+  (let* ((file (buffer-file-name))
+	 (time (string-to-number (format-time-string "%s")))
+	 (latest (aget *koll-latest* file)))
+    (when (and file (or (not latest) (> (- time latest) *koll-interval*)))
+      (aput '*koll-latest* file time)
+      ;; write logfile      
+      (shell-command (concat "echo " (format-time-string "[%Y-%m-%d %H:%M] ") file " >> " *koll-logfile*)))))
+
+(add-hook 'write-file-hooks 'koll-hook)
+
+(provide 'koll)

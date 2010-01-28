fdb3ae09ceaf854982b4732eea402cb76158d7e4
krille <krille@mamma.(none)>
2010-01-28 14:39:09 +0100

    split logfiles by month

--- koll.el
+++ koll.el
@@ -11,16 +11,24 @@
       ;; koll interval is the number of seconds that should pass between 
       ;; log entries of the same file. default 10 minutes.
       *koll-interval* (* 60 10)
-      *koll-logfile* "~/.koll")
+      ;; this must exist
+      *koll-logdir* "~/.koll"
+      *koll-filename* "%Y-%m")
 
-(defun koll-hook ()  
+(unless (file-directory-p *koll-logdir*)
+  (if (file-exists-p *koll-logdir*)
+      (error "*koll-logdir* is a file!")
+    (make-directory *koll-logdir*)))
+
+(defun koll-hook ()
   (let* ((file (buffer-file-name))
 	 (time (string-to-number (format-time-string "%s")))
 	 (latest (aget *koll-latest* file)))
     (when (and file (or (not latest) (> (- time latest) *koll-interval*)))
       ;; write logfile
-      (shell-command (concat "echo " (format-time-string "[%Y-%m-%d %H:%M] ") file " >> " *koll-logfile*))
+      (shell-command (concat "echo " (format-time-string "[%Y-%m-%d %H:%M] ") file " >> " *koll-logdir* "/" (format-time-string *koll-filename*)))
       (aput '*koll-latest* file time)
+      ;; from write-file-hooks doc
       ;; "If one of them returns non-nil, the file is considered already written
       ;; and the rest are not called."
       nil)))
